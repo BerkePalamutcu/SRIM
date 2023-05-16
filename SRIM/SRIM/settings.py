bfe25fe19f60e9f3f4f86d062f904ae9cbe020ad
Berke <berke@winnowpro.com>
2023-05-16 15:15:42 +0300

    add basic templating

--- SRIM/SRIM/settings.py
+++ SRIM/SRIM/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'baseApp.apps.BaseappConfig'
+    'baseApp.apps.BaseappConfig',
 ]
 
 MIDDLEWARE = [
